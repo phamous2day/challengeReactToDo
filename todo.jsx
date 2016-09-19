7fa7a1839961e4a83895dedecf79b0bbf69267a3
David Pham <phamous2day@users.noreply.github.com>
2016-09-19 17:55:15 -0400

    Add files via upload
    It can add tasks, but the new tasks go away. Need a way to retain the tasks.

---
+++ todo.jsx
@@ -0,0 +1,69 @@
+var TodoList = React.createClass({
+  getInitialState: function() {
+    return {
+      newTaskDescription: '',
+
+      //creating some placeholder info as  test
+      tasks: [
+        { id: 1, name: 'loadup furniture'},
+        { id: 2, name: 'make todo list' }
+      ],
+
+
+      //using next helps increment id for next task
+      nextId: 3
+    }
+  },
+  render: function() {
+    return (
+      <div>
+        <form onSubmit={this.addTask}>
+          <input type="text"
+            placeholder="What to do?"
+            value={this.state.newTaskDescription}
+
+            //onChange as event listener for when task descriptions change
+            onChange={this.changeTaskDescription}
+            />
+        </form>
+        <ul>
+          {this.state.tasks.map(function(task) {
+            return (
+
+            //think of key as hash tables/dictionary as a way to retrieve/organize information by id
+
+              <li key={task.id}>
+                {task.name}
+              </li>
+            );
+          }.bind(this))}
+        </ul>
+      </div>
+    );
+  },
+  changeTaskDescription: function(event) {
+    this.setState({
+      newTaskDescription: event.target.value
+    });
+  },
+
+
+  addTask: function(event) {
+    var newItem = {
+      id: this.state.nextId,
+      name: this.state.newTaskDescription
+    };
+    this.setState({
+      tasks: this.state.tasks.concat(newItem),
+      nextId: this.state.nextId + 1,
+      newTaskDescription: ''
+    })
+  }
+});
+
+//Always going to need a render at the bottom, this is one of first components I should already have when project begins
+
+ReactDOM.render(
+  <TodoList/>,
+  document.getElementById('content')
+)
